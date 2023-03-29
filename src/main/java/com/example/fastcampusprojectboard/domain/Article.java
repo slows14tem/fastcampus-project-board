436ccb7456198374b0199b5d5448a7bb70627944
slows14tem <slows14@naver.com>
2023-03-29 18:14:22 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인클래스 == 1 테이블 매칭되도록 설계했다.
    생성자와 equals() and hashcode()등이 도메인 맥락에 맞게끔 디테일하게 설정함

--- src/main/java/com/example/fastcampusprojectboard/domain/Article.java
+++ src/main/java/com/example/fastcampusprojectboard/domain/Article.java
@@ -1,16 +1,84 @@
 package com.example.fastcampusprojectboard.domain;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)  //entity에서도 auditing을 쓴다는 표시
+@Entity
 public class Article {
-    private Long id;
-    private String title;   //제목
-    private String content; //본문
-    private String hashtag; //해시태그
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter private Long id;
 
-    private LocalDateTime createdAt;    //생성일시
-    private String createdBy;   //생성자
-    private LocalDateTime modifiedAt;   //수정일시
-    private String modifiedBy;  //수정자
+    @Setter @Column(nullable = false) private String title;   //제목
+    @Setter @Column(nullable = false, length = 10000) private String content; //본문
+    //@Column은 true가 기본값이고 아무 옵션이 없을때는 생략 가능
+    @Setter private String hashtag; //해시태그
+    //전체 클레스가 아니라 특정 필드에 setter를 주는 이유 : 사용자가 특정 필드를 세팅할 수 없도록 하려고
+    //자동으로 jpa가 세팅할 수 있게 만들기 위함
+
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    @ToString.Exclude    //tostring을 끊어주는 방식
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+    //mappedBy를 안하면 두 엔티티의 이름을 합쳐서 자동생성
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;    //생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;   //생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;   //수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;  //수정자
+    //CreatedDate, CreatedBy, LastModifiedDate, LastModifiedBy = Jpa Auditing 기능
+
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        // of == 도메인 Article을 생성하고자 할때 어떤 값이 필요한지 가이드....
+        return new Article(title, content, hashtag);
+        //factory method???
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;  //pattern matching?? 사용 가능
+        return id != null && id.equals(article.id);
+        //id != null의 의미 = 새로만든, 아직 영속화되지 않은 엔티티는 모두 동등성 검사를 탈락(다 다른값으로 보겠다는 의미)
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    //Collection을 사용하여 리스트로 내보내거나 정렬, 중복제거 등을 할 때 비교가 필요 == 동일성, 동등성을 검사해야함
+    //equals() and hashcode()를 사용
+
 }
+

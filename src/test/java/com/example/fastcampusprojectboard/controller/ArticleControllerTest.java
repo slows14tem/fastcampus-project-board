88b1101344b392e00130cfe5361b43aaf962a9a6
slows14tem <slows14@naver.com>
2023-04-05 21:25:34 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 때문에 적절히 테스트 수정

--- src/test/java/com/example/fastcampusprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/example/fastcampusprojectboard/controller/ArticleControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,7 +24,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -34,7 +32,7 @@ class ArticleControllerTest {
         //When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk()) //정상 호출 체크
-                .andExpect(content().contentType(MediaType.TEXT_HTML))  //컨텐츠 타입 체크
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))  //컨텐츠 타입 체크
                 .andExpect(view().name("articles/index"))   //view의 이름에 데한 테스트
                 .andExpect(model().attributeExists("articles"));    //컨트롤러에서 저장한 모델의 정보를 검증
     }

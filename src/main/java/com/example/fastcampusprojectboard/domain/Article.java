a0ac1eb6d8fdd3bddf9555f637baa4eca7be9cda
slows14tem <slows14@naver.com>
2023-04-11 20:52:18 +0900

    #21 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    테이블 필드 수정으로 영향 받은
    `data.sql` 테스트 데이터도 모두 업데이트
    
    하는 김에 게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용

--- src/main/java/com/example/fastcampusprojectboard/domain/Article.java
+++ src/main/java/com/example/fastcampusprojectboard/domain/Article.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -23,6 +23,8 @@ public class Article  extends AuditingFields{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter private Long id;
 
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount;   // 유저 정보 (ID)
+
     @Setter @Column(nullable = false) private String title;   //제목
     @Setter @Column(nullable = false, length = 10000) private String content; //본문
     //@Column은 true가 기본값이고 아무 옵션이 없을때는 생략 가능
@@ -30,7 +32,7 @@ public class Article  extends AuditingFields{
     //전체 클레스가 아니라 특정 필드에 setter를 주는 이유 : 사용자가 특정 필드를 세팅할 수 없도록 하려고
     //자동으로 jpa가 세팅할 수 있게 만들기 위함
 
-    @OrderBy("id")
+    @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     @ToString.Exclude    //tostring을 끊어주는 방식
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
@@ -38,15 +40,16 @@ public class Article  extends AuditingFields{
 
     protected Article() {}
 
-    private Article(String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
-    public static Article of(String title, String content, String hashtag) {
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
         // of == 도메인 Article을 생성하고자 할때 어떤 값이 필요한지 가이드....
-        return new Article(title, content, hashtag);
+        return new Article(userAccount, title, content, hashtag);
         //factory method???
     }
 
